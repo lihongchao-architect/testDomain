2bf1239b1af59d8b563c9cd5719db230368a9107
503126255@qq.com <lihongchao@ctjsoft.com>
2023-12-15 10:33:01 +0800

    upsource工程初始化

---
+++ Upsource/src/test/java/com/example/upsource/UpsourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpsourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
